dcd54da0929e5690ee9197f14ddb4e7b558aaf57
phy-guanzh <guanzh97@163.com>
2020-03-31 16:48:19 +0800

    fix some bug

--- model/gridpack_submit.py
+++ model/gridpack_submit.py
@@ -14,23 +14,26 @@ if __name__ == "__main__":
         ('49', 'cHq3'),
         ('50', 'cHu'),
         ('51', 'cHd'),
-        ('53', 'cll'),
+#        ('53', 'cll'),
         ('54', 'cll1'),
         ('55', 'cqq1'),
         ('56', 'cqq11'),
         ('57', 'cqq3'),
         ('58', 'cqq31'),
     ]
-     os.system('source source_condor.sh')
-  for ipar,param in enumerate(params):
+
+#    os.system("source source_condor.sh")
+
+    with open('gridpack_submitcondor.sh','w') as outfile:
+        for ipar,param in enumerate(params):
       # 1D cards
       # int
-      process='VBS_SSWW_'+ params[ipar][1]+'_LL'
-      with open('gridpack_submitcondor.sh','w')
-            outfile.write('nohup ./submit_condor_gridpack_generation.sh ' +process+ ' cards/Allcw1/'+process +' >' +process+'.log 2>&1 &' )
+                process='VBS_SSWW_'+ params[ipar][1]+'_LL'
+                outfile.write('nohup ./submit_condor_gridpack_generation.sh ' +process+ ' cards/Allcw1/'+process +' >' +process+'.log 2>&1 & \n' )
 
-      process='VBS_SSWW_'+ params[ipar][1]+'_TL'
-             outfile.write('nohup ./submit_condor_gridpack_generation.sh ' +process+ ' cards/Allcw1/'+process +' >' +process+'.log 2>&1 &' )
+                process='VBS_SSWW_'+ params[ipar][1]+'_TL'
+                outfile.write('nohup ./submit_condor_gridpack_generation.sh ' +process+ ' cards/Allcw1/'+process +' >' +process+'.log 2>&1 & \n' )
 
-        process='VBS_SSWW_'+ params[ipar][1]+'_TT'
-              outfile.write('nohup ./submit_condor_gridpack_generation.sh ' +process+ ' cards/Allcw1/'+process +' >' +process+'.log 2>&1 &' )
+                process='VBS_SSWW_'+ params[ipar][1]+'_TT'
+                outfile.write('nohup ./submit_condor_gridpack_generation.sh ' +process+ ' cards/Allcw1/'+process +' >' +process+'.log 2>&1 & \n' )
+~                                             
